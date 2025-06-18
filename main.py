72f0c06c46e48cd51d996aaad3d2abca35e5ad05
VaibhavDev-18 <vaibhavGitv18@gmail.com>
2025-06-18 23:19:25 +0530

    data validation completed

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.components.data_validation import DataValidation
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 import sys
 
@@ -10,8 +11,16 @@ if __name__ == "__main__":
         training_pipeline_config = TrainingPipelineConfig()
         data_ingestion_config = DataIngestionConfig(training_pipeline_config)
         data_ingestion = DataIngestion(data_ingestion_config)
-        logging.info("Enter try block main.py")
+        logging.info("initiated data ingestion")
         data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+        logging.info("data initiation completed")
         print("data_ingestion_artifact: ", data_ingestion_artifact)
+        data_validation_config = DataValidationConfig(training_pipeline_config)
+        data_validation = DataValidation(data_ingestion_artifact, data_validation_config)
+        logging.info("Initiate data validation")
+        data_validation_artifact = data_validation.initiate_data_validation()
+        logging.info("data validation completed")
+        print("data validation artifact: ", data_validation_artifact)
+
     except Exception as e:
         raise NetworkSecurityException(e, sys)
